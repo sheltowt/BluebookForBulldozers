bc09dbfcf42d9cac53000a6518fda43cb29d5eaa
William Shelton <sheltowt@gmail.com>
2013-03-10 17:34:42 -0700

    Update util.py

--- Benchmark/util.py
+++ Benchmark/util.py
@@ -6,8 +6,8 @@ def get_paths():
     """
     Redefine data_path and submissions_path here to run the benchmarks on your machine
     """
-    data_path = os.path.join("C:", "FastIron", "/home/play/Example/Kaggle/Bulldozer/Data/")
-    submission_path = os.path.join("C:", "FastIron", "/home/play/Example/Kaggle/Bulldozer/Data/")
+    data_path = os.path.join("C:", "FastIron", "/home/play/Example/Kaggle/Bull/Data/")
+    submission_path = os.path.join("C:", "FastIron", "/home/play/Example/Kaggle/Bull/Data/")
     return data_path, submission_path
 
 def get_train_df(data_path = None):
